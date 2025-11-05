a2896bb9d218d418bf64fa77f17de274c6cd6107
levent-ozgn <levent.ozgn@eduge.ch>
2025-11-05 16:03:25 +0200

    added side panel

--- index.js
+++ index.js
@@ -7,22 +7,43 @@
 
 const apikey = WEATHER_API_KEY;
 const countries = document.querySelectorAll("path");
+const sidePanel = document.querySelector("#side-panel");
+const htmlCountry = document.querySelector("#country");
+const htmlDegree = document.querySelector("#degree");
+const htmlHumidity = document.querySelector("#humidity");
+const htmlWeather = document.querySelector("#weather");
+const htmlEmoji = document.querySelector("#emoji");
 
 
 countries.forEach(country =>{
   country.addEventListener("click", function(){
-    countryClass = country.getAttribute('class');
 
+    // S'il n'y a pas de classe, on récupère le name
+    countryClass = country.getAttribute('class');
     if (countryClass === null) {
       countryClass = country.getAttribute('name');
     }
     showWeather(countryClass); 
+    ShowSidePanel();
   })
 })
 
+function HideSidePanel(){
+
+  sidePanel.style.transform = "translateX(-400px)"
+  
+}
+
+function ShowSidePanel(){
+  
+  sidePanel.style.transform = "translateX(0)"
+  sidePanel.style.backgroundColor = "red";
+
+}
+
 
 async function getWeather(city) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&&&appid=${apikey}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
 
   const response = await fetch(url);
 
@@ -35,11 +56,17 @@ async function getWeather(city) {
 
 async function showWeather(city) {
   
+  
     let weatherData = await getWeather(city);
+    // Recupération de meteo
     const weatherDataKelvin = weatherData["main"]["temp"];
-
     
+    // Kelvin à celcius
     let weatherDataC = weatherDataKelvin - 273.15;
     weatherDataC = weatherDataC.toFixed(1);
-    console.log(`The weather of ${city} is ${weatherDataC}`);
+
+    htmlCountry.textContent = city;
+    htmlDegree.textContent = weatherDataC;
+
+    
 }
